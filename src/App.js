9955ffd0b796e3bb3aac1e1411bbd6f5f2c9ed20
Taran <taran@192.168.1.4>
2022-06-23 16:39:35 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import "./i18next";
 import { useTranslation } from "react-i18next";
 
 function App() {
-
   
   const { t, i18n } = useTranslation();
-  function handleClick(lang) {
+  function changeLang(lang) {
     i18n.changeLanguage(lang);
   }
- 
+
+  function showLocation(position) {
+    // var latitude = position.coords.latitude;
+    // var longitude = position.coords.longitude;
+    var locale = navigator.language.split('-')[0];
+    // alert("Latitude : " + latitude + " Longitude: " + longitude + " Language: " + navigator.language);
+    // console.log("Latitude : " + latitude + " Longitude: " + longitude + " Language: " + navigator.language);
+    changeLang(locale);
+  }
+
+  function errorHandler(err) {
+      if(err.code === 1) {
+        alert("Error: Access is denied!");
+      } else if( err.code === 2) {
+        alert("Error: Position is unavailable!");
+      }
+  }
+
+  function getLocation() {
+
+      if(navigator.geolocation) {
+        
+        // timeout at 60000 milliseconds (60 seconds)
+        var options = {timeout:60000};
+        navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
+      } else {
+        alert("Sorry, browser does not support geolocation!");
+      }
+  }
+
+  useEffect(() => {
+    getLocation();
+  });
+
+
   return (
   <main className='nav nav-pills nav-stacked'>
     <nav
@@ -21,10 +54,10 @@ function App() {
           backgroundColor:"transparent"
         }}
       >
-        <button
+        {/* <button
         
           type="button"
-          onClick={() => handleClick("en")}
+          // onClick={() => handleClick("en")}
           className="btn1"
         >
           English
@@ -35,52 +68,59 @@ function App() {
         <button
         
           type="button"
-          onClick={() => handleClick("de")}
+          // onClick={() => handleClick("de")}
           className="btn1"
         >
-          German
+          deutsch
         </button>
         <button
         
           type="button"
-          onClick={() => handleClick("rs")}
+          // onClick={() => handleClick("rs")}
           className="btn1"
         >
-          Russian
+          русский
+        </button>
+        <button
+          type="button"gi
+          // onClick={() => handleClick("ar")}
+          className="btn1"
+        >
+          عربى
         </button>
         <button
           type="button"
-          onClick={() => handleClick("ar")}
+          // onClick={() => handleClick("es")}
           className="btn1"
         >
-          Arabic
-        </button>
-        <button
-          type="button"
-          onClick={() => handleClick("es")}
-          className="btn1"
-        >
-          Spanish
+          español
         </button>
         <button
           
           type="button"
-          onClick={() => handleClick("fr")}
+          // onClick={() => handleClick("fr")}
           className="btn1 "
         >
-          French
+          français
         </button>
+        <p>Buttons not for languages selection.Only avaliable options.</p> */}
       </nav>
     <section className='menu section'>
       <div className="title">
-        <h2>Vibing Miami </h2>
+        <h2 className='year'>{t("established.year")}</h2>
+        <div className='spacing'></div>
+        <h2>{t("title1.title")}</h2>
         <br></br>
-        <h3>Sip good in the hood </h3>
+        <h3>{t("title1.subtitle")} </h3>
         <div className='underline'></div>
+      </div>
+      <div className='spacing'>
+
       </div>
     <div className='section-center'>
     
-      <article key={t("menu1.id")} className='menu-item'>
+      <article
+       key={t("menu1.id")} className='menu-item'>
         <img src={t("menu1.img")} alt={t("menu1.title")} className='photo' />
         <div className="item-info">
           <header>
@@ -141,10 +181,10 @@ function App() {
         </div>
       </article>
       <article key={t("menu7.id")} className='menu-item'>
-        <img src={t("menu1.img")} alt={t("menu7.title")} className='photo' />
+        <img src={t("menu7.img")} alt={t("menu7.title")} className='photo' />
         <div className="item-info">
           <header>
-            <h4>{t("menu1.title")}</h4>
+            <h4>{t("menu7.title")}</h4>
             <h4 className='price'>{t("menu7.price")}</h4>
           </header>
           <p className='item-text'>{t("menu7.desc")}</p>
@@ -164,7 +204,7 @@ function App() {
         <img src={t("menu9.img")} alt={t("menu9.title")} className='photo' />
         <div className="item-info">
           <header>
-            <h4>{t("menu1.title")}</h4>
+            <h4>{t("menu9.title")}</h4>
             <h4 className='price'>{t("menu9.price")}</h4>
           </header>
           <p className='item-text'>{t("menu9.desc")}</p>
